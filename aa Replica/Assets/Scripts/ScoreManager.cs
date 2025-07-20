a792fbe96b1de4772874dddb64279b916f7c1707
SirBucketT <tkpriva@icloud.com>
2025-07-20 22:56:49 +0200

    started work on highscore system

---
+++ aa Replica/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.Serialization;
+
+public class ScoreManager : MonoBehaviour
+{
+    private const string HighscoreKey = "Highscore";
+    [SerializeField] TextMeshProUGUI highScore;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
