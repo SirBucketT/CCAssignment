3cc90b1d57800e9535ad5e10f6d8f499cc260262
SirBucketT <tkpriva@icloud.com>
2025-07-21 01:14:28 +0200

    update

--- aa Replica/Assets/Scripts/GameStarter.cs
+++ aa Replica/Assets/Scripts/GameStarter.cs
@@ -4,12 +4,13 @@ using DG.Tweening;
 public class GameStarter : MonoBehaviour
 {
     [SerializeField] RectTransform topMenuBar;
-    [SerializeField] float moveDuration = 0.5f;
+    [SerializeField] float moveDuration;
     
     void Awake()
     {
         topMenuBar.anchoredPosition = new Vector2(topMenuBar.anchoredPosition.x, 245f);
-
         topMenuBar.DOAnchorPosY(0f, moveDuration).SetEase(Ease.InOutCubic);
+        
+        
     }
 }
