e9af2e2ba9ae4bbfd4d1e7d2a31c82112ebba5b7
SirBucketT <tkpriva@icloud.com>
2025-07-21 00:20:59 +0200

    score update

---
+++ aa Replica/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+           UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+            Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
--- aa Replica/Assets/Scripts/ScoreManager.cs
+++ aa Replica/Assets/Scripts/ScoreManager.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     private const string HighscoreKey = "Highscore";
+    
+    //I know this should be managed else where, but I'm lazy
+    [SerializeField] TextMeshProUGUI HighscoreText;
+
+    void Start()
+    {
+        HighscoreText.text = GetHighscore().ToString();
+    }
     
     // Call this to update the highscore if the new score is higher
     public static void SetNewHighscore(int currentScore)
@@ -20,5 +29,10 @@ public class ScoreManager : MonoBehaviour
     public static int GetHighscore()
     {
         return PlayerPrefs.GetInt(HighscoreKey);
+    }
+    
+    public void ResetHighscore()
+    {
+        PlayerPrefs.DeleteKey(HighscoreKey);
     }
 }
